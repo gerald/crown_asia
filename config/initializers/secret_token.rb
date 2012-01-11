62a152a38b7529c3b084ff6959b82b2769de0361
Gerald Abrencillo <gabrencillo@gmail.com>
2012-01-12 00:52:53 +0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrownAsia::Application.config.secret_token = '965bd7ccc0c3bbe86f772ab5551e5556da713ad49de840db10b8927c427c696caf8c8d45b7d4587a168c3e6f3b65d06a61fb7d8a9e37fe16ee8aab1e93053778'
