3bfa4ae1ec03b64d920d204d85197daea490b73f
gerald <gerald@gerald-laptop.(none)>
2012-01-31 14:44:39 +0800

    Fix error in FG

--- app/helpers/finished_good_transactions_helper.rb
+++ app/helpers/finished_good_transactions_helper.rb
@@ -1,7 +1,7 @@
 module FinishedGoodTransactionsHelper
 
   def lot_number_fg_options
-    FinishedGoodTransaction.all(:include => [:added_bags], :conditions => ["bags.removing_transaction_id IS NULL AND bags.id IS NOT NULL"]).collect{|f| [f.lot_number, f.lot_number]}.uniq!
+    FinishedGoodTransaction.all(:include => [:added_bags], :conditions => ["bags.removing_transaction_id IS NULL AND bags.id IS NOT NULL"]).collect{|f| [f.lot_number, f.lot_number]}.uniq
   end
 
 end
