5225b464df3f9688c20c3f8fd261db91f29a6ed2
Gerald Abrencillo <gabrencillo@gmail.com>
2012-02-02 16:05:52 +0800

    Update for RM quantity

--- app/helpers/raw_material_transactions_helper.rb
+++ app/helpers/raw_material_transactions_helper.rb
@@ -2,7 +2,7 @@ module RawMaterialTransactionsHelper
 
   def lot_number_options(raw_material)
     lot_numbers = RawMaterialTransactionItem.all(:select => "lot_number", :include => [:raw_material_transaction], :conditions => ["raw_material_transactions.raw_material_id = ?", raw_material.id]).collect{|r| r.lot_number}.uniq
-    lot_numbers.collect{|l| ["#{l} - #{raw_material.quantity_on_hand(l)} available", l]}
+    lot_numbers.collect{|l| ["#{l} - #{number_with_precision(raw_material.quantity_on_hand(l), :precision => 5)} available", l]}
   end
 
 end
