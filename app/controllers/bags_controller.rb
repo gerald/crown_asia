02b805f803b943e7db76b9ad059931ac391f370a
Gerald Abrencillo <gabrencillo@gmail.com>
2012-02-15 22:59:28 +0800

    Update return bags

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -2,7 +2,12 @@ class BagsController < ApplicationController
 
   def return
     authorize! :return, Bag
-    @bags = Bag.paginate(:per_page => 20, :page => params[:page], :conditions => ["removing_transaction_id IS NOT NULL"])
+    
+    if params[:lot_number].blank?
+      @bags = Bag.paginate(:per_page => 20, :page => params[:page], :conditions => ["removing_transaction_id IS NOT NULL"])
+    else
+      @bags = Bag.paginate(:include => [:removing_transaction], :per_page => 20, :page => params[:page], :conditions => ["finished_good_transactions.lot_number = ? AND removing_transaction_id IS NOT NULL", params[:lot_number]])
+    end
     
     if request.post?
       @returned_bags = Bag.all(:conditions => ["id IN (?)", params[:bag_ids]])
--- app/helpers/bags_helper.rb
+++ app/helpers/bags_helper.rb
@@ -1,2 +1,11 @@
 module BagsHelper
+
+  def lot_number_bag_options(finished_good = nil)
+    if finished_good
+      FinishedGoodTransaction.all(:include => [:removed_bags], :conditions => ["finished_good_transactions.finished_good_id = ? AND bags.removing_transaction_id IS NOT NULL AND bags.id IS NOT NULL", finished_good.id]).collect{|f| [f.lot_number, f.lot_number]}.uniq
+    else
+      FinishedGoodTransaction.all(:include => [:removed_bags], :conditions => ["bags.removing_transaction_id IS NOT NULL AND bags.id IS NOT NULL"]).collect{|f| [f.lot_number, f.lot_number]}.uniq
+    end
+  end
+
 end
