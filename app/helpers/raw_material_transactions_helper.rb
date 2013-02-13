a0b083e4120981be5df4a0435d1ed1e3582b6ac0
Gerald Abrencillo <gabrencillo@gmail.com>
2013-02-13 16:14:39 +0800

    Fix for RM transaction

--- app/helpers/raw_material_transactions_helper.rb
+++ app/helpers/raw_material_transactions_helper.rb
@@ -1,7 +1,7 @@
 module RawMaterialTransactionsHelper
 
   def lot_number_options(raw_material)
-    lot_numbers = RawMaterialTransactionItem.all(:select => "lot_number", :include => [:raw_material_transaction], :conditions => ["raw_material_transactions.raw_material_id = ?", raw_material.id]).collect{|r| r.lot_number}.uniq
+    lot_numbers = RawMaterialTransactionItem.all(:select => "DISTINCT(lot_number)", :include => [:raw_material_transaction], :conditions => ["raw_material_transactions.raw_material_id = ?", raw_material.id]).collect{|r| r.lot_number}
     lot_numbers = lot_numbers.select{|l| raw_material.quantity_on_hand(l) > 0}
     lot_numbers.collect{|l| ["#{l} - #{raw_material.quantity_on_hand(l)} available", l]}
   end
