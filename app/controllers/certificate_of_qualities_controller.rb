1fb49d2389bff5d6d58858dbfbaef6e85d4f3200
Gerald Abrencillo <gabrencillo@gmail.com>
2013-04-18 00:43:38 +0800

    Update for COQ

--- app/controllers/certificate_of_qualities_controller.rb
+++ app/controllers/certificate_of_qualities_controller.rb
@@ -5,7 +5,35 @@ class CertificateOfQualitiesController < ApplicationController
     
     if request.post? && !params[:lot_number].blank?
       @lot_number = params[:lot_number]
+      @fg_transaction_item = FinishedGoodTransactionItem.includes(:finished_good_transaction).where("lot_number = ? AND finished_good_transactions.finished_good_id = ?", params[:lot_number], @finished_good.id).first
+      
+      @coq = CertificateOfQuality.new
+      @coq.lot_number = params[:lot_number]
+      @coq.finished_good_transaction_id = @fg_transaction_item.finished_good_transaction_id if @fg_transaction_item
+      
+      CoqProperty.where("parent_id IS NULL").all.each do |coq_property|
+        @coq.certificate_of_quality_items.build(:coq_property => coq_property)
+        
+        unless coq_property.children.empty?
+          coq_property.children.each do |child|
+            @coq.certificate_of_quality_items.build(:coq_property => child)
+          end
+        end
+      end
     end
   end
-
+  
+  def create
+    @coq = CertificateOfQuality.new(params[:certificate_of_quality])
+    
+    @finished_good = FinishedGood.find(params[:finished_good_id])
+    @lot_number = @coq.lot_number
+    
+    if @coq.save
+      flash[:notice] = "COQ added for #{@finished_good.name}"
+      redirect_to finished_goods_path
+    else
+      render :action => "search"
+    end
+  end
 end
--- app/models/certificate_of_quality_item.rb
+++ app/models/certificate_of_quality_item.rb
@@ -1,6 +1,7 @@
 class CertificateOfQualityItem < ActiveRecord::Base
 
   belongs_to :certificate_of_quality
+  belongs_to :coq_property
   
   validates :result, :presence => true
 end
