18853e5ee71116f953e9d01bdd1f75c4c856051e
Gerald Abrencillo <gabrencillo@gmail.com>
2013-07-16 14:14:31 +0800

    Update for costing period

--- app/models/raw_material.rb
+++ app/models/raw_material.rb
@@ -57,7 +57,7 @@ class RawMaterial < ActiveRecord::Base
     "#{self.raw_material_type.code} #{self.code}: #{self.name}"
   end
   
-  def last_costing_period_date(costing_date = nil)
+  def last_costing_period_quote_date(costing_date = nil)
     c = CostingPeriod.includes(:costing_period_items)
     c = c.where("costing_period_items.raw_material_id = ? AND costing_period_items.quote IS NOT NULL AND costing_period_items.quote <> ''", self.id)
     c = c.where("costing_periods.costing_date < ?", costing_date) if costing_date
@@ -66,12 +66,30 @@ class RawMaterial < ActiveRecord::Base
     return c.costing_date
   end
   
-  def last_costing_period_price(costing_date = nil)
+  def last_costing_period_quote(costing_date = nil)
     c = CostingPeriodItem.includes(:costing_period)
     c = c.where("costing_period_items.raw_material_id = ? AND costing_period_items.quote IS NOT NULL AND costing_period_items.quote <> ''", self.id)
     c = c.where("costing_periods.costing_date < ?", costing_date) if costing_date
     c = c.order("costing_periods.costing_date DESC").first
     return nil if c.nil?
     return c.quote
+  end
+  
+  def last_costing_period_fg_date(costing_date = nil)
+    c = CostingPeriod.includes(:costing_period_items)
+    c = c.where("costing_period_items.raw_material_id = ? AND costing_period_items.fg IS NOT NULL AND costing_period_items.fg <> ''", self.id)
+    c = c.where("costing_periods.costing_date < ?", costing_date) if costing_date
+    c = c.order("costing_date DESC").first
+    return nil if c.nil?
+    return c.costing_date
+  end
+  
+  def last_costing_period_fg(costing_date = nil)
+    c = CostingPeriodItem.includes(:costing_period)
+    c = c.where("costing_period_items.raw_material_id = ? AND costing_period_items.fg IS NOT NULL AND costing_period_items.fg <> ''", self.id)
+    c = c.where("costing_periods.costing_date < ?", costing_date) if costing_date
+    c = c.order("costing_periods.costing_date DESC").first
+    return nil if c.nil?
+    return c.fg
   end
 end
