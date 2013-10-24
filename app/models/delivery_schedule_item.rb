566e2ad435efc0e4afa4daaafa1ad0829ebc9b78
Gerald Abrencillo <gabrencillo@gmail.com>
2013-10-24 23:12:56 +0800

    Remove FG transaction when DS item is deleted

--- app/models/delivery_schedule_item.rb
+++ app/models/delivery_schedule_item.rb
@@ -5,21 +5,21 @@ class DeliveryScheduleItem < ActiveRecord::Base
   # belongs_to :finished_good
   belongs_to :item, :polymorphic => true
   
-  has_many :finished_good_transactions
+  has_many :finished_good_transactions, :dependent => :destroy
   
   validates :customer, :item, :quantity, :presence => true
   validates :quantity, :numericality => true
   validates :po, :so, :length => {:maximum => 30}, :allow_nil => true, :allow_blank => true
   
-  after_destroy :return_removed_bags
+  # after_destroy :return_removed_bags
   
   protected
   
-  def return_removed_bags
-    self.finished_good_transactions.each do |fgt|
-      fgt.removed_bags.each do |bag|
-        bag.return
-      end
-    end
-  end
+  # def return_removed_bags
+#     self.finished_good_transactions.each do |fgt|
+#       fgt.removed_bags.each do |bag|
+#         bag.return
+#       end
+#     end
+#   end
 end
